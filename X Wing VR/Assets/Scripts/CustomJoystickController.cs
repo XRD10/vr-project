dd7247cae93006caa1f2921ad3cafe3de9119c3c
Simon <simon.mayer@lego.com>
2024-11-08 17:28:14 +0100

    stuff

---
+++ X Wing VR/Assets/Scripts/CustomJoystickController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace UnityEngine.XR.Content.Interaction
+{
+    public class CustomJoystickController : XR.Interaction.Toolkit.Interactables.XRBaseInteractable
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
