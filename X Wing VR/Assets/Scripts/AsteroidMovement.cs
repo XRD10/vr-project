9c805161ab977e954e694850043470cd7018fde7
David Kocur <62095094+Davidek776@users.noreply.github.com>
2024-11-13 08:50:45 +0100

    Add asteroid rotation.

--- X Wing VR/Assets/Scripts/AsteroidMovement.cs
+++ X Wing VR/Assets/Scripts/AsteroidMovement.cs
@@ -1,43 +1,12 @@
 using UnityEngine;
-using System.Collections;
 
 public class AsteroidMovement : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed;
-    [SerializeField] private float moveSpeed = 5f;
 
-    private Transform xWing;
 
     void Start()
     {
         GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * rotationSpeed;
-
-        GameObject xWingObject = GameObject.FindGameObjectWithTag("X-Wing");
-
-        if (xWingObject != null)
-        {
-            xWing = xWingObject.transform;
-        }
-        else
-        {
-            Debug.LogWarning("No X-Wing GameObject found with tag 'X-Wing'");
-        }
-
-        StartCoroutine(DestroyAsteroidAfterTime(40f));
-    }
-
-    void Update()
-    {
-        if (xWing != null)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, xWing.position, moveSpeed * Time.deltaTime);
-        }
-    }
-
-    private IEnumerator DestroyAsteroidAfterTime(float time)
-    {
-        yield return new WaitForSeconds(time);
-
-        Destroy(gameObject);
     }
 }
