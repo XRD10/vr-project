9ff70390f250f9e330df91465ffb7d604ea9c6c2
James Cullen <94792906+JcullenNZ@users.noreply.github.com>
2024-11-19 18:03:26 +0100

    Merged into main
    Merged into main scene

--- X Wing VR/Assets/Scripts/PlayerHealth.cs
+++ X Wing VR/Assets/Scripts/PlayerHealth.cs
@@ -6,13 +6,12 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth;
     public int currentHealth;
 
-    public Transform spawnLocation;
+    public Vector3 spawnLocation;
 
     private void Start()
     {
         currentHealth = maxHealth;
-        spawnLocation = transform;
-        
+        spawnLocation = gameObject.transform.position;
     }
 
     public void TakeDamage(int damage)
@@ -30,8 +29,8 @@ public class PlayerHealth : MonoBehaviour
     private IEnumerator Reset()
     {
         yield return new WaitForSeconds(3);
-        gameObject.transform.position = spawnLocation.position;
-        gameObject.transform.rotation = spawnLocation.rotation;
+        gameObject.transform.position = spawnLocation;
+        gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
         currentHealth = maxHealth;
     }
 }
