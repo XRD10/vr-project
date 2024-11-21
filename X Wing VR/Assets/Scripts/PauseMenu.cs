f3df60307f567ee82ff9c1898feb5950f146da5c
David Kocur <62095094+Davidek776@users.noreply.github.com>
2024-11-21 18:55:59 +0100

    Work on adding functionality to pause menu.

---
+++ X Wing VR/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public static PauseMenu instance;
+    public Button continueButton;
+    public Button menuButton;
+
+    [SerializeField]
+    private GameObject pauseMenu;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        pauseMenu.SetActive(true);
+        continueButton.onClick.AddListener(HidePauseMenu);
+        menuButton.onClick.AddListener(NavigateToMainMenu);
+    }
+
+    public void ShowPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    public void HidePauseMenu()
+    {
+        Debug.Log("Hide Pause Menu");
+
+    }
+
+    public void NavigateToMainMenu()
+    {
+        SceneLoader.instance.LoadScene("Menu");
+    }
+
+}
